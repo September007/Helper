a8c5360f727bd03a65965ef3c128552fd56720bd
xucl <xucl@300624.cn>
2023-01-17 17:33:31 +0800

    set mold

--- include/Helper/project.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-/* generate by configure_file */
-
-#define CMAKE_BUILD_TYPE Debug
-#define CMAKE_BINARY_DIR  "/home/lull/src/CODE/cuda-test/build"
-#define CMAKE_SOURCE_DIR "/home/lull/src/CODE/cuda-test"
-#define ASSET_DIR  "/home/lull/src/CODE/cuda-test/Asset"
